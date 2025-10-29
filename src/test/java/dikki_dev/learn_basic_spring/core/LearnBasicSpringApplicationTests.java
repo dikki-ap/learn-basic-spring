a08f80de84343c34ac5659a58cb26083aaab1345
Dikki AP <dikki.agung@epson.co.id>
2025-10-29 09:19:52 +0700

    Initial Spring Template

---
+++ src/test/java/dikki_dev/learn_basic_spring/core/LearnBasicSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dikki_dev.learn_basic_spring.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnBasicSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
